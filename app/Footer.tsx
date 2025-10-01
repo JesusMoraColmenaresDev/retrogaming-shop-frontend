023cf8853fce9add60d06fec1cbba7b364755256
Jesus Miguel Mora Colmenares <jesusmiguelmora.dev@gmail.com>
2025-10-01 09:29:03 -0400

    footer added

---
+++ app/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DynamicLink from './components/dynamic/DynamicLink'
+
+export default function Footer() {
+    return (
+        //ponle un border-b al p de color : #7F13EC80 y porfavor de fondo por #7F13EC10
+        <footer className="w-full bg-[#1a1a1a] text-white py-4">
+            <div className="mx-auto flex flex-col justify-between items-center">
+                <div className="flex space-x-4 mt-2 md:mt-0  w-full items-center justify-center border-b-2 border-[#7F13EC80] pb-4">
+                    <DynamicLink href="/about" text="Acerca de" />
+                    <DynamicLink href="/contact" text="Contacto" />
+                    <DynamicLink href="/privacy" text="Política de Privacidad" />
+                </div>
+                <p className="text-center p-4 text-sm ">{new Date().getFullYear()} Mi Aplicación. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    )
+}
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+"use client";
+import React, { use } from 'react'
+
+import { useRouter } from 'next/navigation';
+import DynamicItemCard from '../components/dynamic/DynamicItemCard';
+import DynamicButton from '../components/dynamic/DynamicButton';
 
 export default function GamesPage() {
+  const router = useRouter();
   return (
-    <div>Games</div>
+    <main>
+      <section className="my-20 flex flex-col gap-8 items-center justify-center">
+        <h2 className="text-white text-3xl font-bold">Catalogo de juegos</h2>
+        <h3 className="text-gray-400 text-xl">Explora nuestra amplia selección de juegos retro</h3>
+        <div className="flex gap-8 w-full justify-center">
+          <DynamicItemCard
+            imageUrl="/retro-gaming-setup-stockcake.jpg"
+            title="Game Title"
+            subtitle="Game Description"
+            button={<DynamicButton text="Ver juego" type="button" onClick={() => router.push('/games')} className="px-8" />}
+          />
+          <DynamicItemCard
+            imageUrl="/retro-gaming-setup-stockcake.jpg"
+            title="Game Title"
+            subtitle="Game Description"
+            button={<DynamicButton text="Ver juego" type="button" onClick={() => router.push('/games')} className="px-8" />}
+          />
+          <DynamicItemCard
+            imageUrl="/retro-gaming-setup-stockcake.jpg"
+            title="Game Title"
+            subtitle="Game Description"
+            button={<DynamicButton text="Ver juego" type="button" onClick={() => router.push('/games')} className="px-8" />}
+          />
+          <DynamicItemCard
+            imageUrl="/retro-gaming-setup-stockcake.jpg"
+            title="Game Title"
+            subtitle="Game Description"
+            button={<DynamicButton text='Ver juego' type='button' onClick={() => router.push('/consoles')} className='px-8' ></DynamicButton>}
+
+          />
+        </div>
+      </section>
+    </main>
   )
 }
